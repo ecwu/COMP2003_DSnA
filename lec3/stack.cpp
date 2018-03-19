1fe449ee98b885d9aa62d0d533fb0ea3a55d3b2b
ecwu <bitlabwzh@gmail.com>
2018-03-19 22:26:05 +0800

    move main function to main.cpp

--- lec3/stack.cpp
+++ lec3/stack.cpp
@@ -73,26 +73,3 @@ void DestroyStack(Stack* stack){
 	free(stack -> values);
 	stack -> top = -1; // reset the stack top to -1 (empty)
 }
-
-int main(void) {
-	Stack stack;
-	double val;
-
-	CreateStack(&stack, 5);
-	Push(&stack, 5);
-	Push(&stack, 6.5);
-	Push(&stack, -3);
-	Push(&stack, -8);
-	DisplayStack(&stack);
-	if(Top(&stack, &val))
-		printf("Top: %g\n", val);
-	Pop(&stack, &val);
-	if(Top(&stack, &val))
-		printf("Top: %g\n", val);
-	while(!IsEmpty(&stack))
-		Pop(&stack, &val);
-	DisplayStack(&stack);
-	DestroyStack(&stack);
-
-	return 0;
-}
