74f376c18060393b0fae3e0bbe61e09df0182636
ecwu <bitlabwzh@gmail.com>
2018-03-27 11:10:58 +0800

    add comments

--- lec4/queue.cpp
+++ lec4/queue.cpp
@@ -16,7 +16,7 @@ bool CreateQueue(Queue *queue, int size){
 }
 
 bool IsEmpty(Queue* queue){
-	if (queue->counter == 0){
+	if (queue->counter == 0){ // if counter is equal to zero: queue is empty
 		return true;
 	}else{
 		return false;
@@ -24,7 +24,7 @@ bool IsEmpty(Queue* queue){
 }
 
 bool IsFull(Queue* queue){
-	if (queue -> counter == queue -> maxSize){
+	if (queue -> counter == queue -> maxSize){ // if counter is equal to maximum size: queue is full
 		return true;
 	}else{
 		return false;
@@ -34,11 +34,11 @@ bool IsFull(Queue* queue){
 
 bool Enqueue(Queue* queue, double x){
 	if(IsFull(queue)){
-		printf("Error: the queue is full.\n");
+		printf("Error: the queue is full.\n"); // queue can't enqueue if the queue is full
 		return false;
 	}
-	queue -> rear = (queue -> rear + 1) % queue -> maxSize;
+	queue -> rear = (queue -> rear + 1) % queue -> maxSize; // Circular increase index
 	queue -> values[queue -> rear] = x;
-	queue -> counter++;
+	queue -> counter++; // modify counter to indicate the queue status
 	return true;
-}bool Dequeue(Queue* queue, double* x){	if(IsEmpty(queue)){		return false;	}	*x = queue -> values[queue -> front];	queue -> front = (queue -> front + 1) % queue -> maxSize;	queue -> counter--;	return true;}void DisplayQueue(Queue* queue){	if(IsEmpty(queue)){		printf("The queue is empty.\n");	}	for (int i = 0; i < queue -> counter; i++){		int currentIndex = ((queue -> front + i) % (queue -> maxSize));		if (currentIndex == queue -> front){			printf("front -->\t");		}else{			printf("\t\t");		}		printf("%g", queue->values[currentIndex]);		if(currentIndex == queue -> rear){			printf("\t<-- rear\n");		}else{			printf("\n");		}	}}void DestroyQueue(Queue* queue){	free(queue->values);}
+}bool Dequeue(Queue* queue, double* x){	if(IsEmpty(queue)){ // queue can't dequeue if the queue is empty		return false;	}	*x = queue -> values[queue -> front];	queue -> front = (queue -> front + 1) % queue -> maxSize; // Circular decrease index	queue -> counter--; // modify counter to indicate the queue status	return true;}void DisplayQueue(Queue* queue){	if(IsEmpty(queue)){		printf("The queue is empty.\n");	}	for (int i = 0; i < queue -> counter; i++){		int currentIndex = ((queue -> front + i) % (queue -> maxSize)); // use front, i and maxSize to calculate the correct index		if (currentIndex == queue -> front){ // if the current index is the front index			printf("front -->\t"); // print a front message		}else{			printf("\t\t"); // otherwise just print two tab		}		printf("%g", queue->values[currentIndex]); // print out the queue value according to index		if(currentIndex == queue -> rear){ // if the current index is the rear index			printf("\t<-- rear\n"); // print a rear message		}else{			printf("\n"); // otherwise just print a line break		}	}}void DestroyQueue(Queue* queue){	free(queue->values); // free the queue values array}
